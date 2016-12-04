45c434aa4e004aa5be9d600404b61ebd15290e34
Hubert Łępicki <hubert.lepicki@amberbit.com>
2016-12-04 07:00:16 +0100

    [10] introduce cond, regexps

--- lib/chatbot/commands.ex
+++ lib/chatbot/commands.ex
@@ -1,6 +1,14 @@
 defmodule Chatbot.Commands do
   @hodor_speak [ "Hodor", "...hodor", "Hodor?", "Hodor, hodor!" ]
 
+  def execute({:howdy, _text}) do
+    "I'm good, thank you! And you?"
+  end
+
+  def execute({:hi, _text}) do
+    "Hello, my friend!"
+  end
+
   def execute(_) do
     Enum.random(@hodor_speak)
   end
--- lib/chatbot/parser.ex
+++ lib/chatbot/parser.ex
@@ -1,5 +1,9 @@
 defmodule Chatbot.Parser do
   def parse(text) do
-    {:unknown, text}
+    cond do
+      String.match?(text, ~r/hi|hello|hola|hey/i) -> {:hi, text}
+      String.match?(text, ~r/how are you|what's up/i) -> {:howdy, text}
+      true -> {:unknown, text}
+    end
   end
 end
